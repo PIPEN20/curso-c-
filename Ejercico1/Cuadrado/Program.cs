2b0ee74a39ad9ed6a20ea3e0eabe493a2dbbe838
PIPEN <andresfaquino@gmail.com>
2023-10-18 22:40:56 -0400

    Tarea1

--- Ejercico1/Cuadrado/Program.cs
+++ Ejercico1/Cuadrado/Program.cs
@@ -1,1 +1,7 @@
-﻿
+﻿int side = 0;
+
+Console.WriteLine("ingrese medida de un lado: ");
+string line = Console.ReadLine();
+side = (Convert.ToInt32(line) * 4);
+
+Console.WriteLine("El perimetro del cuadrado es: " + side);
--- Ejercico1/suma y promedio/Program.cs
+++ Ejercico1/suma y promedio/Program.cs
@@ -1,1 +1,26 @@
 ﻿
+
+int num1 = 0;
+int num2 = 0;
+int num3 = 0;
+int num4 = 0;
+
+Console.WriteLine("ingrese primer numero: ");
+string line = Console.ReadLine();
+num1 = Convert.ToInt32(line);
+
+Console.WriteLine("ingrese segundo numero: ");
+line = Console.ReadLine();
+num2 = Convert.ToInt32(line);
+
+Console.WriteLine("ingrese tercer numero: ");
+line = Console.ReadLine();
+num3 = Convert.ToInt32(line);
+
+Console.WriteLine("ingrese cuarto numero: ");
+line = Console.ReadLine();
+num4 = Convert.ToInt32(line);
+
+Console.WriteLine("El resultado de la suma de estos numeros es igual a: " + (num1 + num2 + num3 + num4)); 
+Console.WriteLine("Elpromedo de estos numeros es igual a: " + ((num1 + num2 + num3 + num4) / 4));
+
